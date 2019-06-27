34260995ef466aa6f994b93aa6a052af3450aae6
falstack <icesilt@outlook.com>
2019-06-27 17:19:38 +0800

    update

--- examples/Container.vue
+++ examples/Container.vue
@@ -1,5 +1,5 @@
 <style lang="scss">
-.v-container {
+.v-scroller {
   height: 100%;
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
@@ -7,17 +7,23 @@
 </style>
 
 <template>
-  <div class="v-container" @scroll="handleScroll">
-    <slot/>
+  <div
+    class="v-scroller"
+    @scroll="handleScroll"
+    @touchstart="handleStart"
+    @touchmove="handleMove"
+  >
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Container',
+  name: 'VScroller',
   data() {
     return {
-      lastScrollTop: 0
+      lastScrollTop: 0,
+      lastTouchY: 0
     }
   },
   methods: {
@@ -25,23 +31,44 @@ export default {
       const scrollTop = evt.target.scrollTop
       const isUp = this.lastScrollTop > scrollTop
       if (isUp) {
-        if (scrollTop < 50) {
+        if (scrollTop < 50 && scrollTop > 0) {
           this.$emit('pull-down')
         }
       } else {
-        if (scrollTop < 50) {
+        if (scrollTop < 50 && scrollTop > 0) {
           this.$emit('pull-up')
         }
         if (this.$el.scrollHeight - this.$el.clientHeight - scrollTop < 50) {
           this.$emit('bottom')
         }
       }
+
       this.lastScrollTop = scrollTop
-      console.log(scrollTop)
       this.$emit('scroll', {
         offset: scrollTop,
         isUp
       })
+    },
+    handleStart(evt) {
+      this.lastTouchY = evt.touches[0].pageY
+      this.fixedIOS()
+    },
+    handleMove(evt) {
+      if (evt.touches[0].pageY > this.lastTouchY && this.lastScrollTop <= 0) {
+        this.$emit('pull-down')
+      }
+    },
+    fixedIOS() {
+      const el = this.$el
+      let top = el.scrollTop
+      let totalScroll = el.scrollHeight
+      let currentScroll = top + el.offsetHeight
+
+      if (top === 0) {
+        el.scrollTop = 1
+      } else if (currentScroll === totalScroll) {
+        el.scrollTop = top - 1
+      }
     }
   }
 }
