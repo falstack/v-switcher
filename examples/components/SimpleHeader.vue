f5505786dc0ff5872fd2cb551f315a1c62cb2912
falstack <icesilt@outlook.com>
2019-08-07 20:51:01 +0800

    add: vuepress

---
+++ examples/components/SimpleHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-switcher :headers="headers">
+    <div
+      v-for="(item, index) in headers"
+      :key="index"
+      :slot="`${index}`"
+    >
+      {{ item }}
+    </div>
+  </v-switcher>
+</template>
+
+<script>
+export default {
+  name: 'SimpleHeader',
+  computed: {
+    headers() {
+      return ['游戏', '动漫', '话题']
+    }
+  }
+}
+</script>
