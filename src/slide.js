2a2a311d69e9b0a0b51c8938a79bd5ac483ca351
falstack <icesilt@outlook.com>
2019-08-23 12:14:56 +0800

    update: slide.js

--- src/slide.js
+++ src/slide.js
@@ -14,39 +14,58 @@ export default class {
     this._setupIndex()
     this._setupStyle()
     this._setupTouchEvents()
-    this._setupResizeEvent()
     return this
   }
 
   prev(custom = true) {
-    if (this.activeIndex === 0 || this.moving) {
+    if (this.moving || (this.activeIndex === 0 && !this.continuous)) {
+      return
+    }
+    if (this.activeIndex === 0 && this.continuous) {
+      this._continuousAnimation(false)
       return
     }
     if (custom) {
       this.activeIndex--
-    } else if (this._isValidSlide()) {
-      this._calcActiveIndex(false)
+    } else {
+      if (this._isValidSlide()) {
+        this._calcActiveIndex(false)
+        this._animation()
+        return
+      }
+      this._animation(false)
     }
-    this._animation()
   }
 
   next(custom = true) {
-    if (this.activeIndex === this.slideCount - 1 || this.moving) {
+    if (
+      this.moving ||
+      (this.activeIndex === this.slideCount - 1 && !this.continuous)
+    ) {
+      return
+    }
+    if (this.activeIndex === this.slideCount - 1 && this.continuous) {
+      this._continuousAnimation(true)
       return
     }
     if (custom) {
       this.activeIndex++
-    } else if (this._isValidSlide()) {
-      this._calcActiveIndex(true)
+    } else {
+      if (this._isValidSlide()) {
+        this._calcActiveIndex(true)
+        this._animation()
+        return
+      }
+      this._animation(false)
     }
-    this._animation()
   }
 
   slide(index) {
     if (
+      this.moving ||
+      index === this.activeIndex ||
       index < 0 ||
-      index > this.slideCount - 1 ||
-      index === this.activeIndex
+      index > this.slideCount - 1
     ) {
       return
     }
@@ -54,8 +73,67 @@ export default class {
     this._animation()
   }
 
+  scroll(left) {
+    this.moving = true
+    const { cssPrefix, duration, style } = this
+    requestAnimationFrame(() => {
+      style[`${cssPrefix}transition-duration`] = `${duration}ms`
+      style[`${cssPrefix}transform`] = `translateX(${left}px)`
+      setTimeout(() => {
+        style[`${cssPrefix}transition-duration`] = ''
+        this.currentLeft = left
+        this.lastLeft = left
+        this.moving = false
+      }, duration)
+    })
+  }
+
+  update(options = {}) {
+    if (!this.cssPrefix) {
+      return
+    }
+    if (this.adjust) {
+      this.slideCount = this.el.children.length
+    }
+    if (options) {
+      if (options.index !== undefined) {
+        this.activeIndex = options.index
+      }
+      if (options.maxLeft !== undefined) {
+        this.maxLeft = Math.abs(options.maxLeft)
+      }
+    }
+    this.destroy()
+    this._setupSizes()
+    this._setupIndex()
+    this._setupTouchEvents()
+  }
+
+  play(timeout = 1000) {
+    const time = Math.max(timeout, this.duration)
+    if (this.autoplayTimer) {
+      return
+    }
+    this.continuous = true
+    this.sticky = false
+    this.autoplayTimer = setInterval(() => {
+      this.next()
+    }, time)
+  }
+
+  stop() {
+    if (!this.autoplayTimer) {
+      return
+    }
+    clearInterval(this.autoplayTimer)
+    this.autoplayTimer = 0
+  }
+
   destroy() {
     const { el, events } = this
+    if (!el) {
+      return
+    }
     el.removeEventListener('touchstart', events.touchstart, {
       capture: true,
       passive: true
@@ -65,14 +143,14 @@ export default class {
       capture: true,
       passive: true
     })
-    window.removeEventListener('resize', events.resize, {
-      capture: false,
-      passive: true
-    })
+    this.stop()
   }
 
   _setupTouchEvents() {
     const { el, events } = this
+    if (!el) {
+      return
+    }
     events.touchstart = this._start.bind(this)
     events.touchmove = this._move.bind(this)
     events.touchend = this._end.bind(this)
@@ -83,14 +161,6 @@ export default class {
     el.addEventListener('touchmove', events.touchmove, true)
     el.addEventListener('touchend', events.touchend, {
       capture: true,
-      passive: true
-    })
-  }
-
-  _setupResizeEvent() {
-    this.events.resize = this._setupSizes.bind(this)
-    window.addEventListener('resize', this.events.resize, {
-      capture: false,
       passive: true
     })
   }
@@ -113,29 +183,38 @@ export default class {
     this.moving = false
     this.startAt = 0
     this.events = {}
+    this.autoplayTimer = 0
   }
 
   _setupProps(options) {
+    const duration = options.duration === undefined ? 300 : Math.abs(options.duration)
     this.el = options.el
     this.style = options.el.style
-    this.duration =
-      options.duration === undefined ? 300 : Math.abs(options.duration)
-    this.sticky = options.sticky === undefined ? true : options.sticky
+    this.duration = duration
+    this.sticky = duration ? (options.sticky === undefined ? true : options.sticky) : false
     this.swipe = options.swipe === undefined ? true : options.swipe
     this.disabled = options.disabled || false
+    this.continuous = options.continuous || false
+    this.adjust = options.adjust === undefined ? true : options.adjust
+    this.maxLeft = options.maxLeft ? Math.abs(options.maxLeft) : 0
+    this.autoCalcMaxLeft = !options.maxLeft
     this.callback = options.callback
-    this.slideCount = Math.max(options.count || 1, 1)
+    this.beforeCallback = options.beforeCallback
+    this.slideCount = Math.max(options.el.children.length, 1)
     this.activeIndex = options.index
       ? Math.max(Math.min(options.index, this.slideCount - 1), 0)
       : 0
   }
 
   _setupStyle() {
+    if (!this.adjust) {
+      return
+    }
     const { style, slideCount, slideWidth } = this
     style.width = `${slideCount * 100}%`
     if (slideCount > 1) {
-      const children = this.el.children
-      ;[].forEach.call(children, item => {
+      const slides = this.el.children
+      ;[].forEach.call(slides, item => {
         const { style } = item
         style.width = `${slideWidth}px`
         style.float = 'left'
@@ -154,9 +233,14 @@ export default class {
   }
 
   _setupSizes() {
+    if (!this.el) {
+      return
+    }
     const offsetWidth = this.el.parentNode.offsetWidth
     this.slideWidth = offsetWidth
-    this.maxLeft = offsetWidth * this.slideCount - offsetWidth
+    if (this.autoCalcMaxLeft) {
+      this.maxLeft = offsetWidth * this.slideCount - offsetWidth
+    }
   }
 
   _start(event) {
@@ -228,19 +312,49 @@ export default class {
     }
   }
 
-  _animation() {
+  _continuousAnimation(isNext) {
     this.moving = true
-    const { cssPrefix, duration, activeIndex } = this
-    const left = -activeIndex * this.slideWidth
+    const { cssPrefix, slideCount, duration, slideWidth, style, el, beforeCallback } = this
+    const slides = el.children
+    const slide = isNext ? slides[0] : slides[slideCount - 1]
+    const left = isNext ? -slideCount * slideWidth : slideWidth
+    const activeIndex = isNext ? 0 : slideCount - 1
+    slide.style[`${cssPrefix}transform`] = `translateX(${(isNext ? 100 : -100) *
+      slideCount}%)`
+    beforeCallback && beforeCallback(activeIndex)
     requestAnimationFrame(() => {
-      this.style[`${cssPrefix}transition-duration`] = `${duration}ms`
-      this.style[`${cssPrefix}transform`] = `translateX(${left}px)`
+      style[`${cssPrefix}transition-duration`] = `${duration}ms`
+      style[`${cssPrefix}transform`] = `translateX(${left}px)`
       setTimeout(() => {
-        this.style[`${cssPrefix}transition-duration`] = ''
+        const finalLeft = isNext ? 0 : -activeIndex * slideWidth
+        style[`${cssPrefix}transition-duration`] = ''
+        style[`${cssPrefix}transform`] = `translateX(${finalLeft}px)`
+        slide.style[`${cssPrefix}transform`] = ''
+        this.currentLeft = finalLeft
+        this.lastLeft = finalLeft
+        this.activeIndex = activeIndex
+        this.moving = false
+        this.callback && this.callback(activeIndex)
+      }, duration)
+    })
+  }
+
+  _animation(emit = true) {
+    this.moving = true
+    const { cssPrefix, duration, activeIndex, style, beforeCallback } = this
+    const left = -activeIndex * this.slideWidth
+    beforeCallback && beforeCallback(activeIndex)
+    requestAnimationFrame(() => {
+      style[`${cssPrefix}transition-duration`] = `${duration}ms`
+      style[`${cssPrefix}transform`] = `translateX(${left}px)`
+      setTimeout(() => {
+        style[`${cssPrefix}transition-duration`] = ''
         this.currentLeft = left
         this.lastLeft = left
         this.moving = false
-        this.callback && this.callback(activeIndex)
+        if (emit) {
+          this.callback && this.callback(activeIndex)
+        }
       }, duration)
     })
   }
@@ -269,6 +383,7 @@ export default class {
     let result = ''
     const regex = /^(Webkit|Khtml|Moz|ms|O)(?=[A-Z])/
     const styleDeclaration = document.getElementsByTagName('script')[0].style
+    // eslint-disable-next-line no-unused-vars
     for (const prop in styleDeclaration) {
       if (regex.test(prop)) {
         result = '-' + prop.match(regex)[0].toLowerCase() + '-'
